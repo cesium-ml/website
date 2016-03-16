d421ac93ece46a8eccb1f6021d81a8c96f97eec3
Stefan van der Walt <stefanv@berkeley.edu>
2016-03-16 10:50:41 -0700

    Update upload tool to fetch/reset correctly
    Also copy contents of output folder, not output folder itself

--- tools/gh-upload.sh
+++ tools/gh-upload.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+set -e
+
 THIS_DIR="$( dirname "${BASH_SOURCE[0]}")"
 ROOT=$(cd "${THIS_DIR}/.." && pwd)
 
 cd $ROOT/gh-pages
-cp -r ../blog/output ./blog
+git fetch origin
+git reset --hard origin/master
+cp -r ../blog/output/* ./blog
 git add .
 git commit -m "Update blog"
 git push origin master
